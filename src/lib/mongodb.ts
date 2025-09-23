4caa5f02a3d391771deedc74c6a95a0a736701fc
Josephat Ongwae <ongwaebrowser7@gmail.com>
2025-09-23 19:27:17 +0000

    Make sure you use URI for mongodb as in the code "const { MongoClient, S

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,7 +6,13 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options = {
+  serverApi: {
+    version: 'v1' as const,
+    strict: true,
+    deprecationErrors: true,
+  }
+};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
